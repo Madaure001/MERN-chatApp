4d02f1b0593fa9eb7bcaa77346174573fe6d6171
Madaure Thulani <calnganyi@gmail.com>
2024-03-08 13:28:38 +0200

    second part completed -design

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,17 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+  return (
+    <>
+        <div className="py-2 flex flex-col overflow-auto"></div>
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+    </>
+  )
+}
+
+export default Conversations;
