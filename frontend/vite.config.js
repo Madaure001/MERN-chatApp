9add0a6d6bd71c75057bb97b8a968c4956a914d9
Madaure Thulani <calnganyi@gmail.com>
2024-03-12 20:56:46 +0200

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5000,
     proxy: {
       "/api": {
-        target: "https://chatlyap.netlify.app/"
+        target: "http://localhost:5000/"
       },
     },
   },
