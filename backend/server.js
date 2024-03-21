a68eacaf599b7c73ab52ec13d913d60ca16acd8b
Madaure Thulani <calnganyi@gmail.com>
2024-03-21 14:33:13 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,9 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 //middleware
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist/")));
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+    res.sendFile(path.join(__dirname, "frontend", "dist/src", "index.html"))
 })
 
 
